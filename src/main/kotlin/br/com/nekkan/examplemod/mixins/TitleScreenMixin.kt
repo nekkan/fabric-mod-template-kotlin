17924df25625b6caa43d512b3477c390fed61178
nekkan <nxkkdev@gmail.com>
2021-03-01 14:25:30 -0300

    feat: Create TitleScreenMixin.kt

---
+++ src/main/kotlin/br/com/nekkan/examplemod/mixins/TitleScreenMixin.kt
@@ -0,0 +1,17 @@
+package br.com.nekkan.examplemod.mixins
+
+import net.minecraft.client.gui.screen.TitleScreen
+import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.injection.At
+import org.spongepowered.asm.mixin.injection.Inject
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+
+@Mixin(TitleScreen::class)
+class TitleScreenMixin {
+
+    @Inject(at = [At("HEAD")], method = ["init()V"])
+    fun init(callbackInfo: CallbackInfo) {
+        println("Hello from TitleScreen.class!")
+    }
+
+}
